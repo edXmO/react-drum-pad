cf422e388f7103f5f9828cb160d3cd2dce2ae1cd
Eduardo <ex.romeroma@gmail.com>
2021-02-19 11:03:58 +0100

    toggle on/off and volume btn functionality added

--- src/components/Control/Control.js
+++ src/components/Control/Control.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../css/App.css';
 import Display from './Display/Display';
 import Panel from './Panel/Panel';
 import Copy from './Copy/Copy';
 
 const Control = () => {
+
+    const [toggle, setToggle] = useState(false);
+    const [volume, setVolume] = useState(80);
+
+    const handleToggle = () => {
+        setToggle(prevState => !prevState);
+    }
+
+
     return (
         <div className='control'>
-            <Display type={'VOL: 80%'} screenType={'display display__volume'} />
-            <Panel />
-            <Display type={'Hi hat'} screenType={'display display__sound'} />
+            <Display type={`VOL: ${volume}%`} screenType={`display display__volume ${toggle ? 'display display__volume--on' : ''}`} />
+            <Panel toggle={toggle} setVolume={setVolume} handleToggle={handleToggle} />
+            <Display type={'Hi hat'} screenType={`display display__sound ${toggle ? 'display display__sound--on' : ''}`} />
             <Copy />
         </div>
     )
--- src/components/Control/Panel/Button/Button.js
+++ src/components/Control/Panel/Button/Button.js
@@ -1,12 +1,38 @@
 import React from 'react';
 
-const Button = ({ text, type }) => {
-    return (
-        <div className='btn-panel__box'>
-            <button className='btn-panel__btn'></button>
-            {text}
-        </div>
-    )
+const Button = ({ setVolume, handleToggle, text, type, on }) => {
+
+    const handleVolume = () => {
+        if (text === '+VOL') {
+            setVolume(prevState => prevState + 1);
+        }
+        if (text === '-VOL') {
+            setVolume(prevState => prevState - 1);
+        }
+    }
+
+    const handleClickSwitch = () => {
+        handleToggle();
+    }
+
+    if (type === 'toggle') {
+        return (
+            <div className={`btn-panel__box ${on ? 'btn-panel__box--on' : ''}`}>
+                <button onClick={handleClickSwitch} className='btn-panel__btn'></button>
+                {on ? 'ON' : 'OFF'}
+            </div>
+        )
+    }
+    if (type === 'volume') {
+        return (
+            <div className={`btn-panel__box ${type === 'toggle' ? 'btn-panel__box--on' : ''}`}>
+                <button onClick={handleVolume} className='btn-panel__btn'></button>
+                {text}
+            </div>
+        )
+    }
 }
+
+
 
 export default Button;
